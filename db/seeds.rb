ab233882121c7b6735817772c8b381d37806d90c
TJBachorz <TJBachorz@gmail.com>
2020-09-01 13:38:15 -0600

    fixes planets database

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,29 @@ def get_data
     response = RestClient.get("https://api.le-systeme-solaire.net/rest/bodies/")
     data = JSON.parse(response)
     planets =  data['bodies'].select do |body|
-        body['isPlanet'] == true
+        body['isPlanet'] == true && !body['name'].match(/\d/) 
     end
     planets    
 end
 
 def create_planets
     get_data.each do |planet|
-        Planet.create(name: planet["englishName"], moons: planet["moons"] ? planet["moons"].length : 0, diameter: (planet["meanRadius"]*2), gravity: (planet["gravity"]/9.8).round(2))
+        Planet.create(
+            name: planet["englishName"], 
+            moons: planet["moons"] ? planet["moons"].length : 0, 
+            diameter: (planet["meanRadius"]*2), 
+            gravity: (planet["gravity"]/9.8).round(2)
+            )
     end
 end
 
 create_planets
 
+# def filter_planets(planets)
+#     planets.select do |planet|
+#     # byebug
+#     planet['name'].match(/\d/)    
+#     end
+# end
+
+# selection = filter_planets(planets)
